9a6ad5539153f8bf7ee3630a768a3eb899e74a42
1Jae <wjkim@linkhub.co.kr>
2021-05-12 18:48:08 +0900

    v1.13.0 added DeleteBankAccount API

--- EasyFin/EasyFinBankService.cs
+++ EasyFin/EasyFinBankService.cs
@@ -109,6 +109,21 @@ namespace Popbill.EasyFin
             return httppost<Response>(uri, CorpNum, null, null, null, UserID);
         }
         
+        // 종량제 계좌 삭제
+        public Response DeleBankAccount(string CorpNum, EasyFinBankAccountForm info, string UserID = null)
+        {
+            if (info == null) throw new PopbillException(-99999999, "은행 계좌정보가 입력되지 않았습니다.");
+            if (info.BankCode == null || info.BankCode == "") throw new PopbillException(-99999999, "은행코드가 입력되지 않았습니다.");
+            if (info.BankCode.Length != 4) throw new PopbillException(-99999999, "은행코드가 올바르지 않습니다.");
+            if (info.AccountNumber == null || info.AccountNumber == "") throw new PopbillException(-99999999, "은행 계좌번호가 입력되지 않았습니다.");
+
+            string PostData = toJsonString(info);
+
+            string uri = "/EasyFin/Bank/BankAccount/Delete";
+
+            return httppost<Response>(uri, CorpNum, PostData, null, null, UserID);
+        }
+
         public EasyFinBankAccount GetBankAccountInfo(string CorpNum, string BankCode, string AccountNumber)
         {
             return GetBankAccountInfo(CorpNum, BankCode, AccountNumber, null);
