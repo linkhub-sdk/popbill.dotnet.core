f1e11e8e34604884ef6a630a8d715c840ec411a8
박지헌 <jhpark@linkhubcorp.com>
2024-02-26 17:58:21 +0900

    PaymentResponse type 오류 수정

--- Base/PaymentResponse.cs
+++ Base/PaymentResponse.cs
@@ -5,7 +5,7 @@ namespace Popbill
     [DataContract]
     public class PaymentResponse
     {
-        [DataMember] public string code;
+        [DataMember] public long code;
         [DataMember] public string message;
         [DataMember] public string settleCode;
     }
